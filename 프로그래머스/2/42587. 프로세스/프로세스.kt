3924843af22bac89b31c07f2ba14fcb5fd0c69ae
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-04-21 23:38:05 +0900

    [level 2] Title: 프로세스, Time: 19.42 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.kt
@@ -0,0 +1,36 @@
+class Solution {
+    fun solution(priorities: IntArray, location: Int): Int {
+        var answer = 0
+        val arr = ArrayList<Pair<Int,Int>>()
+
+        // 우선순위와 알파벳(0 == A, 1 == B..) 저장
+        for(i in priorities.indices){
+            arr.add(Pair(priorities[i],i))
+        }
+
+        // 큐가 다 빌때까지
+        while(arr.isNotEmpty()){
+            // 정렬하여 최우선순위 값 확인
+            val tmpArr = arr.sortedByDescending { it.first }
+            val maxPrior = tmpArr[0].first
+            
+            // arrayList 의 첫번째 원소를 사용함으로써 큐처럼 구현
+            if(arr[0].first != maxPrior){
+                // 우선순위가 아니라면 맨뒤로 뺌
+                arr.add(arr[0])
+            }
+            else{
+                // 우선순위라면 출력하고 (answer++) 
+                answer++
+                // 그 값이 찾는 값이라면 break
+                if(arr[0].second == location){
+                    break
+                }
+            }
+            // 맨 앞의 값 제거 (pop)
+            arr.removeAt(0)
+        }
+
+        return answer
+    }
+}
