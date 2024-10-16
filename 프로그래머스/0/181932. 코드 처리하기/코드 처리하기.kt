650526dc2d9eb628552111de883e0f7f9660ec16
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-10-16 20:28:39 +0900

    [level 0] Title: 코드 처리하기, Time: 371.57 ms, Memory: 358 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(code: String): String {
+        var answer: String = ""
+        var mode:Boolean= true
+        for (i in 0 until code.length) {  
+            if(mode){
+                if (code[i] == '1') {
+                    mode = !mode  
+                } else if (i % 2 == 0) {
+                    answer+=code[i]
+                }   
+            }
+            else {
+                if (code[i] == '1') {
+                    mode = !mode  
+                } else if (i % 2 == 1) {
+                    answer+=code[i]
+                }
+            }
+        }
+        
+          return if (answer.isEmpty()) "EMPTY" else answer.toString()
+    }
+}
