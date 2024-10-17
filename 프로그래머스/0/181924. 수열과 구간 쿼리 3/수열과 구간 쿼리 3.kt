b64bc0050e3c98a43640a22292b1d91f61a1fe9c
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-10-17 11:41:12 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.10 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.kt
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.kt
@@ -1,13 +1,6 @@
 class Solution {
     fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
         var answer: IntArray = intArrayOf()
-        answer = arr
-        queries.forEach{
-            var temp= answer[it[0]]
-            answer[it[0]] = answer[it[1]]
-            
-            answer[it[1]] = temp
-        }
         return answer
     }
 }
