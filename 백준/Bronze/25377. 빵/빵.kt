4e8a948c4bcb42309c13bd9ac3ae4482ecd8f50d
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-11-08 02:13:10 +0900

    [Bronze IV] Title: 빵, Time: 124 ms, Memory: 21536 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val rp = readLine()!!.toInt()
+    var min = 1001
+
+    repeat(rp) {
+        val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+        if (a < b) min = if (min > b) b else min
+    }
+
+    if (min == 1001) {
+        print("-1")
+    } else {
+        print(min)
+    }
+}
